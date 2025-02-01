3ae0554b152516a478d5458ba2b412ca083a2735
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2025-02-01 14:02:27 -0600

    feat(drawer): fun fact drawer added

---
+++ src/components/FunFact.tsx
@@ -0,0 +1,94 @@
+'use client'
+import { Button, type ButtonProps } from '@/components/ui/button'
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from '@/components/ui/drawer'
+import { contentData } from '@/data/contentData'
+import { VisuallyHidden } from 'radix-ui'
+import { forwardRef, useState } from 'react'
+import parse from 'html-react-parser'
+
+const FunFactButton = forwardRef<HTMLButtonElement, ButtonProps>(
+    ({ className, ...props }, ref) => {
+        return (
+            <Button
+                {...props}
+                className={`bg-funFactBackground hover:bg-funFactBackground text-[#9FD71C] focus:outline-none focus:border-none font-black rounded-none rounded-t-2xl px-10 py-8 text-3xl translate-y-[1px] ${className}`}
+            >
+                FUN FACT
+            </Button>
+        )
+    }
+)
+
+export interface FunFactDrawerProps {
+    selectedFootprint: keyof typeof contentData
+}
+export default function FunFactDrawer({
+    selectedFootprint,
+}: FunFactDrawerProps) {
+    const [open, setOpen] = useState(false)
+    const selectedFootprintData = contentData[selectedFootprint]
+    const drawerTriggerOffset = {
+        left: 'left-5',
+        right: 'right-5',
+    }
+    return (
+        <div
+            className={`flex justify-end absolute ${drawerTriggerOffset.left} ${drawerTriggerOffset.right} bottom-0`}
+        >
+            <Drawer direction="bottom" open={open} onOpenChange={setOpen}>
+                <DrawerTrigger asChild>
+                    <div className="flex flex-col pl-4 pr-4 w-full">
+                        {/* Dinosaur Silhouette */}
+                        <div className="relative basis-1/3">
+                            <div className="absolute -bottom-16 right-64">
+                                <img
+                                    src={selectedFootprintData.silhouetteSrc}
+                                    alt="Sauropod silhouette"
+                                    className="object-contain"
+                                />
+                            </div>
+                        </div>
+                        <FunFactButton className="self-end" />
+                        <div className=" bg-funFactBackground h-14 rounded-tl-4xl "></div>
+                    </div>
+                </DrawerTrigger>
+                <DrawerContent className="bg-funFactBackground text-white border-none left-[34.5rem] right-[78px] rounded-tl-3xl rounded-tr-none rounded-bl-none">
+                    <VisuallyHidden.Root>
+                        <DrawerTitle>Fun Fact</DrawerTitle>
+                    </VisuallyHidden.Root>
+                    <DrawerClose asChild>
+                        <FunFactButton
+                            className={`absolute right-0 -top-16 ${!open ? 'hidden' : 'visible'}`}
+                        />
+                    </DrawerClose>
+                    <DrawerClose>
+                        <div className="mx-auto px-20 pt-20 pb-10 flex gap-4">
+                            <DrawerHeader hidden>
+                                <DrawerDescription
+                                    className="text-white/90 space-y-4 text-xl"
+                                    asChild
+                                >
+                                    <div className="flex flex-col gap-2">
+                                        {parse(selectedFootprintData.funFact)}
+                                    </div>
+                                </DrawerDescription>
+                            </DrawerHeader>
+                            <img
+                                className="w-[528px] h-[380px] bg-white/20 rounded-lg mb-4"
+                                src={selectedFootprintData.funFactSrc}
+                            />
+                        </div>
+                    </DrawerClose>
+                </DrawerContent>
+            </Drawer>
+        </div>
+    )
+}
--- src/components/dinosaur-footprint-viewer.tsx
+++ src/components/dinosaur-footprint-viewer.tsx
@@ -3,6 +3,7 @@
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
 import { contentData } from '@/data/contentData'
+import FunFactDrawer from './FunFact'
 
 export interface DinosaurFootprintViewerProps {
     selectedFootprint: keyof typeof contentData
@@ -12,78 +13,75 @@ export default function DinosaurFootprintViewer({
 }: DinosaurFootprintViewerProps) {
     const currentFootprintDetail = contentData[selectedFootprint]
     return (
-        <div className="flex gap-6 p-6 bg-gray-900 text-white min-h-screen">
+        <div className="flex pb-0 bg-gray-900 text-white min-h-screen">
             {/* Sidebar */}
-            <div className="w-72 flex flex-col gap-4">
-                <Card className="p-6 bg-emerald-900 text-lime-400">
-                    <h1 className="text-4xl font-bold mb-4">Technology WOW!</h1>
-                    <p className="text-sm">
+            <div className="w-[480px] flex flex-col gap-4 p-10 bg-sidebar-pattern">
+                <Card className="p-10 bg-black/50 text-lime-400 pb-32 border-none rounded-3xl">
+                    <h1 className="mb-8 font-commissioner leading-[80px] flex flex-col">
+                        <span className="text-[53px]  font-semibold inline-block">
+                            Technology
+                        </span>{' '}
+                        <span className="text-[97px] font-bold inline-block self-end">
+                            WOW!
+                        </span>
+                    </h1>
+                    <p className="text-2xl leading-9 text-white px-4">
                         High-tech tools can create digital models of fossils.
                         Now scientists can preserve, share, and study these
                         traces of the past in new ways.
                     </p>
+                    <div className="flex flex-col gap-8 mt-20">
+                        {Object.entries(contentData).map(
+                            ([id, footprintDetail]) => (
+                                <Button
+                                    key={id}
+                                    variant={
+                                        selectedFootprint === id
+                                            ? 'secondary'
+                                            : 'ghost'
+                                    }
+                                    className={`w-full justify-start ${selectedFootprint === id ? 'bg-[#9FD71C]' : 'bg-lime-600/70'} rounded-3xl py-14 text-center text-black`}
+                                    onClick={() =>
+                                        (window.location.href = `/${id}`)
+                                    }
+                                >
+                                    <div className="text-xl leading-6 w-full">
+                                        <div className="font-bold ">
+                                            {footprintDetail.title}
+                                        </div>
+                                        <div className="font-medium">
+                                            {footprintDetail.location}
+                                        </div>
+                                    </div>
+                                </Button>
+                            )
+                        )}
+                    </div>
                 </Card>
-
-                <div className="space-y-2">
-                    {Object.entries(contentData).map(
-                        ([id, footprintDetail]) => (
-                            <Button
-                                key={id}
-                                variant={
-                                    selectedFootprint === id
-                                        ? 'secondary'
-                                        : 'ghost'
-                                }
-                                className="w-full justify-start bg-lime-600/20 hover:bg-lime-600/30 text-left"
-                                onClick={() =>
-                                    (window.location.href = `/${id}`)
-                                }
-                            >
-                                <div>
-                                    <div className="font-medium">
-                                        {footprintDetail.title}
-                                    </div>
-                                    <div className="text-xs text-lime-400/70">
-                                        {footprintDetail.location}
-                                    </div>
-                                </div>
-                            </Button>
-                        )
-                    )}
-                </div>
             </div>
 
             {/* Main Content */}
-            <div className="flex-1 space-y-4">
-                <div className="relative aspect-video w-full bg-gray-800 rounded-lg overflow-hidden">
+            <div className="flex-1 space-y-10">
+                <div className="relative ">
                     <img
                         src={currentFootprintDetail.src}
                         alt="3D scan of a dinosaur footprint"
-                        className="object-cover"
+                        className="object-cover w-full max-h-[803px]"
                     />
                 </div>
 
                 {/* Info Box */}
-                <div className="flex gap-2 bg-lime-500 p-6 rounded-lg pr-48 h-48">
+                <div className="flex gap-2 bg-lime-500 p-10 rounded-t-[40px] pr-48 h-60 relative ml-10 mr-10">
                     <div className="flex flex-col gap-2 flex-grow">
-                        <h2 className="text-xl font-bold text-gray-900 mb-2">
+                        <h2 className="text-2xl font-semibold text-gray-900 mb-2">
                             {currentFootprintDetail.detailTitle}
                         </h2>
-                        <p className="text-gray-900">
+                        <p className="text-gray-900 text-xl w-[698px]">
                             {currentFootprintDetail.detailDescription}
                         </p>
                     </div>
 
-                    {/* Dinosaur Silhouette */}
-                    <div className="relative basis-1/3">
-                        <div className="w-32 h-16 absolute bottom-12 right-4">
-                            <img
-                                src={currentFootprintDetail.silhouetteSrc}
-                                alt="Sauropod silhouette"
-                                className="object-contain"
-                            />
-                        </div>
-                    </div>
+                    <FunFactDrawer selectedFootprint={selectedFootprint} />
                 </div>
             </div>
         </div>
