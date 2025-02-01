a9a106f8ef6c4ababa82ab4664b8cee8e89c0022
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2025-02-01 10:35:25 -0600

    feat(pagerouting): basic page routing and content switching

---
+++ src/data/contentData.ts
@@ -0,0 +1,38 @@
+export const contentData = {
+    sauropod: {
+        detailId: 'sauropod',
+        title: 'Sauropod Footprint',
+        src: '/footprints/sauropod.png',
+        silhouetteSrc: '/silhouettes/sauropod.svg',
+        location: 'Dinosaur Valley State Park',
+        detailTitle: 'Earth-shaking Sauropod',
+        detailDescription:
+            'Due to its enormous size, scientists think that a Sauroposeidon made this track. Named for the Greek God of oceans and earthquakes, this long-necked dinosaur may have made the ground shake with each step!',
+        media: '3D Photogrammetry, 2023',
+        credit: 'Courtesy Paul Baker',
+    },
+    theropod: {
+        detailId: 'theropod',
+        title: 'Theropod Footprint',
+        src: '/footprints/theropod.png',
+        silhouetteSrc: '/silhouettes/theropod.svg',
+        location: 'Dinosaur Valley State Park',
+        detailTitle: 'Terrifying Theropod',
+        detailDescription:
+            'This footprint was likely made by a theropod, a group of bipedal dinosaurs that includes the terrifying Tyrannosaurus rex. Theropods were carnivorous and walked on two legs.',
+        media: '3D Photogrammetry, 2023',
+        credit: 'Courtesy Paul Baker',
+    },
+    footprint3: {
+        detailId: 'footprint3',
+        title: 'Footprint #3',
+        src: '/footprints/footprint3.png',
+        silhouetteSrc: '/silhouettes/footprint3.svg',
+        location: 'Dinosaur Valley State Park',
+        detailTitle: 'Footprint #3',
+        detailDescription:
+            'This is a description of Footprint #3. It was made by a dinosaur.',
+        media: '3D Photogrammetry, 2023',
+        credit: 'Courtesy Paul Baker',
+    },
+} as const
