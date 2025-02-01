0cd6f52a926fbd8ae0b83124813745a8d2af57ab
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2025-02-01 14:02:27 -0600

    feat(drawer): fun fact drawer added

--- src/data/contentData.ts
+++ src/data/contentData.ts
@@ -10,6 +10,9 @@ export const contentData = {
             'Due to its enormous size, scientists think that a Sauroposeidon made this track. Named for the Greek God of oceans and earthquakes, this long-necked dinosaur may have made the ground shake with each step!',
         media: '3D Photogrammetry, 2023',
         credit: 'Courtesy Paul Baker',
+        funFact:
+            '<p>During the summer of 2022, a drought dried the Puluxy River in Glen Rose, Texas briefly revealing many never-before-seen dinosaur footprints.</p><p>A team of volunteers from Dallas Paleontological Society volunteers, Glen Kuban, and Paul Baker rushed to clean, record, and measure the tracks before the drought ended.</p>',
+        funFactSrc: '/footprints/sauropod.png',
     },
     theropod: {
         detailId: 'theropod',
@@ -22,6 +25,9 @@ export const contentData = {
             'This footprint was likely made by a theropod, a group of bipedal dinosaurs that includes the terrifying Tyrannosaurus rex. Theropods were carnivorous and walked on two legs.',
         media: '3D Photogrammetry, 2023',
         credit: 'Courtesy Paul Baker',
+        funFact:
+            '<p>During the summer of 2022, a drought dried the Puluxy River in Glen Rose, Texas briefly revealing many never-before-seen dinosaur footprints.</p><p>A team of volunteers from Dallas Paleontological Society volunteers, Glen Kuban, and Paul Baker rushed to clean, record, and measure the tracks before the drought ended.</p>',
+        funFactSrc: '/footprints/sauropod.png',
     },
     footprint3: {
         detailId: 'footprint3',
@@ -34,5 +40,8 @@ export const contentData = {
             'This is a description of Footprint #3. It was made by a dinosaur.',
         media: '3D Photogrammetry, 2023',
         credit: 'Courtesy Paul Baker',
+        funFact:
+            '<p>During the summer of 2022, a drought dried the Puluxy River in Glen Rose, Texas briefly revealing many never-before-seen dinosaur footprints.</p><p>A team of volunteers from Dallas Paleontological Society volunteers, Glen Kuban, and Paul Baker rushed to clean, record, and measure the tracks before the drought ended.</p>',
+        funFactSrc: '/footprints/sauropod.png',
     },
 } as const
