badc6bc87245e2b324630fbf5f8e067221a65e95
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2025-02-09 12:37:20 -0600

    fix(objmodel): rotation was not working

--- src/components/ObjModelViewer.tsx
+++ src/components/ObjModelViewer.tsx
@@ -1,90 +1,83 @@
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState, useEffect, useCallback } from 'react'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js'
 
 export interface ObjModelViewerProps {
     modelFileName: string
 }
 export const ObjModelViewer = ({ modelFileName }: ObjModelViewerProps) => {
-    const thisRef = useRef({})
-    const mountRef = useRef()
-    const initialDate = useRef(Date.now())
+    const thisRef = useRef<
+        Partial<{
+            camera: THREE.PerspectiveCamera
+            scene: THREE.Scene
+            renderer: THREE.WebGLRenderer
+            controls: OrbitControls
+            model: THREE.Object3D
+            requestID: number
+            quaternion: THREE.Quaternion
+            startAnimationLoop: FrameRequestCallback
+        }>
+    >({})
+
+    const mountRef = useRef<HTMLDivElement>(null)
     const [modelResetTimerIsRunning, setModelResetTimerIsRunning] =
         useState(false)
-    const [userInteractingWithModel, setUserInteractingWithModel] =
-        useState(false)
-
-    const resetModelPosition = () => {
-        thisRef.current.camera.position.x = 0
-        thisRef.current.camera.position.y = 0
-        thisRef.current.camera.position.z = 1
-        thisRef.current.camera.updateProjectionMatrix()
-        thisRef.current.controls.update()
-        initialDate.current = Date.now()
-        setModelResetTimerIsRunning(false)
-    }
-
-    useEffect(() => {
-        const timer = setInterval(() => {
-            // check whether we should reset the model after user interacts with it
-            if (modelResetTimerIsRunning) {
-                resetModelPosition()
-            }
-        }, 1000)
-        return () => clearInterval(timer)
-    }, [modelResetTimerIsRunning])
-
-    useEffect(() => {
-        thisRef.current.sceneSetup()
-        thisRef.current.addLights()
-        thisRef.current.loadTheModel()
-        thisRef.current.startAnimationLoop()
-        window.addEventListener('resize', thisRef.current.handleWindowResize)
-        return () => {
-            window.removeEventListener(
-                'resize',
-                thisRef.current.handleWindowResize
-            )
-            window.cancelAnimationFrame(thisRef.current.requestID)
-            thisRef.current.controls.dispose()
-        }
-    }, [])
 
     // Standard scene setup in Three.js. Check "Creating a scene" manual for more information
     // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
-    thisRef.current.sceneSetup = () => {
-        // get container dimensions and use them for scene sizing
-        const width = mountRef.current.clientWidth
-        const height = mountRef.current.clientHeight
+    const sceneSetup = useCallback(() => {
+        if (mountRef.current) {
+            // get container dimensions and use them for scene sizing
+            const width = mountRef.current.clientWidth
+            const height = mountRef.current.clientHeight
 
-        thisRef.current.scene = new THREE.Scene()
-        thisRef.current.scene.background = new THREE.Color(0x686868)
-        thisRef.current.camera = new THREE.PerspectiveCamera(
-            75, // fov = field of view
-            width / height, // aspect ratio
-            0.1, // near plane
-            1000 // far plane
-        )
-        // thisRef.current.camera.position.z = 2; // is used here to set some distance from a cube that is located at z = 0
-        // OrbitControls allow a camera to orbit around the object
-        // https://threejs.org/docs/#examples/controls/OrbitControls
-        thisRef.current.controls = new OrbitControls(
-            thisRef.current.camera,
-            mountRef.current
-        )
-        thisRef.current.renderer = new THREE.WebGLRenderer({ antialias: true })
-        thisRef.current.renderer.shadowMap.type = THREE.PCFSoftShadowMap
-        thisRef.current.renderer.outputEncoding = THREE.sRGBEncoding
-        thisRef.current.renderer.toneMapping = THREE.LinearToneMapping
-        thisRef.current.renderer.setSize(width, height)
-        mountRef.current?.appendChild(thisRef.current.renderer.domElement) // mount using React ref
-    }
+            thisRef.current.scene = new THREE.Scene()
+            thisRef.current.scene.background = new THREE.Color(0x686868)
+            thisRef.current.camera = new THREE.PerspectiveCamera(
+                75, // fov = field of view
+                width / height, // aspect ratio
+                0.1, // near plane
+                1000 // far plane
+            )
+            thisRef.current.camera.position.z = 1 // is used here to set some distance from a cube that is located at z = 0
+            // OrbitControls allow a camera to orbit around the object
+            // https://threejs.org/docs/#examples/controls/OrbitControls
+            thisRef.current.controls = new OrbitControls(
+                thisRef.current.camera,
+                mountRef.current
+            )
+            thisRef.current.controls.autoRotate = true
+            thisRef.current.controls.autoRotateSpeed = 5
+            thisRef.current.renderer = new THREE.WebGLRenderer({
+                antialias: true,
+            })
+            thisRef.current.renderer.shadowMap.type = THREE.PCFSoftShadowMap
+            thisRef.current.renderer.toneMapping = THREE.LinearToneMapping
+            thisRef.current.renderer.setSize(width, height)
+            mountRef.current.appendChild(thisRef.current.renderer.domElement) // mount using React ref
+        }
+    }, [])
 
-    // Code below is taken from Three.js OBJ Loader example
-    // https://threejs.org/docs/#examples/en/loaders/OBJLoader
-    thisRef.current.loadTheModel = () => {
+    // adding some lights to the scene
+    const addLights = useCallback(() => {
+        if (thisRef.current.scene) {
+            const ambientLight = new THREE.AmbientLight(0xffffff)
+            ambientLight.castShadow = true
+            ambientLight.intensity = 1.5
+            thisRef.current.scene.add(ambientLight)
+
+            // add some extra light, gives a bit of a texture pop
+            const extraLight = new THREE.DirectionalLight()
+            extraLight.position.set(0, 2000, 1000)
+            extraLight.intensity = 0.5
+            extraLight.castShadow = true
+            thisRef.current.scene.add(extraLight)
+        }
+    }, [])
+
+    const loadTheModel = useCallback(() => {
         const mtlLoader = new MTLLoader()
         // instantiate a loader
         const loader = new OBJLoader()
@@ -100,7 +93,7 @@ export const ObjModelViewer = ({ modelFileName }: ObjModelViewerProps) => {
                 )
                 .then(
                     (object) => {
-                        thisRef.current.scene.add(object)
+                        thisRef.current.scene?.add(object)
 
                         // centering the model
                         // THREE.Box3() is AABB (axis-aligned bounding box). You can set it from the object you've loaded.
@@ -119,107 +112,119 @@ export const ObjModelViewer = ({ modelFileName }: ObjModelViewerProps) => {
                         thisRef.current.quaternion = quaternion
                     },
                     // called when loading is in progresses
-                    (xhr) => {
+                    (xhr: ProgressEvent) => {
                         const loadingPercentage = Math.ceil(
                             (xhr.loaded / xhr.total) * 100
                         )
                         console.log(loadingPercentage + '% loaded')
-                    },
-                    // called when loading has errors
-                    (error) => {
-                        console.log('An error happened:' + error)
                     }
                 )
         })
+    }, [])
+
+    const resetModelPosition = () => {
+        if (thisRef.current.camera) {
+            updateCameraPosition({ x: 0, y: 0, z: 1 })
+        }
+        setModelResetTimerIsRunning(false)
     }
 
-    // adding some lights to the scene
-    thisRef.current.addLights = () => {
-        // let lights = [];
+    useEffect(() => {
+        const timer = setInterval(() => {
+            // check whether we should reset the model after user interacts with it
+            if (modelResetTimerIsRunning) {
+                resetModelPosition()
+            }
+        }, 10000)
+        return () => clearInterval(timer)
+    }, [modelResetTimerIsRunning])
 
-        // // // set color and intensity of lights
-        // lights[0] = new THREE.PointLight(0xffffff, 1, 0);
-        // lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-        // lights[2] = new THREE.PointLight(0xffffff, 1, 0);
-        // lights.forEach((light) => {
-        //   light.castShadow = false;
-        //   // light.intensity = 0.7;
-        //   light.decay = 2;
-        // });
+    useEffect(() => {
+        if (thisRef.current) {
+            sceneSetup?.()
+            addLights?.()
+            loadTheModel?.()
+            thisRef.current.startAnimationLoop?.(0)
+            if (handleWindowResize) {
+                window.addEventListener('resize', handleWindowResize)
+                return () => {
+                    if (handleWindowResize) {
+                        window.removeEventListener('resize', handleWindowResize)
+                    }
+                    window.cancelAnimationFrame(thisRef.current.requestID ?? 0)
+                    thisRef.current.controls?.dispose()
+                }
+            }
+        }
+    }, [sceneSetup])
 
-        // // // place some lights around the scene for best looks and feel
-        // lights[0].position.set(0, 2000, 0);
-        // lights[1].position.set(1000, 2000, 1000);
-        // lights[2].position.set(-1000, -2000, -1000);
+    const updateCameraPosition = ({
+        x,
+        y,
+        z,
+    }: { x?: number; y?: number; z?: number } = {}) => {
+        if (thisRef.current.camera) {
+            if (x || y || z) {
+                thisRef.current.camera.position.x = x || 0
+                thisRef.current.camera.position.y = y || 0
+                thisRef.current.camera.position.z = z || 0
+            }
+            thisRef.current.camera.lookAt(0, 0, 0)
+            thisRef.current.controls?.update()
+            thisRef.current.camera.updateProjectionMatrix()
 
-        // thisRef.current.scene.add(lights[0]);
-        // thisRef.current.scene.add(lights[1]);
-        // thisRef.current.scene.add(lights[2]);
-
-        const ambientLight = new THREE.AmbientLight(0x777777)
-        // ambientLight.castShadow = true;
-        ambientLight.intensity = 1.5
-        thisRef.current.scene.add(ambientLight)
-
-        // add some extra light, gives a bit of a texture pop
-        const extraLight = new THREE.DirectionalLight()
-        extraLight.position.set(0, 2000, 1000)
-        extraLight.intensity = 0.2
-        thisRef.current.scene.add(extraLight)
+            if (thisRef.current.renderer && thisRef.current.scene) {
+                thisRef.current.renderer.render(
+                    thisRef.current.scene,
+                    thisRef.current.camera
+                )
+            }
+        }
     }
 
     thisRef.current.startAnimationLoop = () => {
         // slowly rotate an object
 
         // Rotate camera around the model
-        if (
-            thisRef.current.camera &&
-            !userInteractingWithModel &&
-            !modelResetTimerIsRunning
-        ) {
-            const radius = 1
-            const speed = 0.0005
-            const time = (Date.now() - initialDate.current) * speed
-
-            thisRef.current.camera.position.x = radius * Math.cos(time)
-            thisRef.current.camera.position.z = radius * Math.sin(time)
-            thisRef.current.camera.lookAt(0, 0, 0)
-            thisRef.current.controls.update()
-        }
-
-        thisRef.current.renderer.render(
-            thisRef.current.scene,
-            thisRef.current.camera
-        )
+        // if (!modelResetTimerIsRunning) {
+        updateCameraPosition()
+        // }
 
         // The window.requestAnimationFrame() method tells the browser that you wish to perform
         // an animation and requests that the browser call a specified function
         // to update an animation before the next repaint
-        thisRef.current.requestID = window.requestAnimationFrame(
-            thisRef.current.startAnimationLoop
-        )
+        if (thisRef.current.startAnimationLoop) {
+            thisRef.current.requestID = window.requestAnimationFrame(
+                thisRef.current.startAnimationLoop
+            )
+        }
     }
 
-    thisRef.current.handleWindowResize = () => {
-        const width = mountRef.current.clientWidth
-        const height = mountRef.current.clientHeight
-
-        thisRef.current.renderer.setSize(width, height)
-        thisRef.current.camera.aspect = width / height
-
-        // Note that after making changes to most of camera properties you have to call
-        // .updateProjectionMatrix for the changes to take effect.
-        thisRef.current.camera.updateProjectionMatrix()
-    }
+    const handleWindowResize = useCallback(() => {
+        const width = mountRef.current?.clientWidth
+        const height = mountRef.current?.clientHeight
+        if (
+            width &&
+            height &&
+            thisRef.current.renderer &&
+            thisRef.current.camera
+        ) {
+            thisRef.current.renderer.setSize(width, height)
+            thisRef.current.camera.aspect = width / height
+            // Note that after making changes to most of camera properties you have to call
+            // .updateProjectionMatrix for the changes to take effect.
+            thisRef.current.camera.updateProjectionMatrix()
+        }
+    }, [])
 
     return (
         <div
-            onMouseDown={() => {
-                setUserInteractingWithModel(true)
+            onMouseDown={(event) => {
+                return event
             }}
-            onMouseUp={() => {
+            onMouseUp={(event) => {
                 setModelResetTimerIsRunning(true)
-                setUserInteractingWithModel(false)
+                return event
             }}
             className="w-full h-[803px]"
             ref={mountRef}
